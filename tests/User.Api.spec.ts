c09eebc7d4d043aa38967f3a89734edb6083c06d
AndrewKhv <a.d.khramchenkov@gmail.com>
2025-02-10 20:24:56 +0100

    feat: TL-14 test fixes

--- tests/User.Api.spec.ts
+++ tests/User.Api.spec.ts
@@ -5,11 +5,11 @@ import { APIRequestContext } from 'playwright'
 
 test.describe('User managment app tests', async () => {
   const clearUser = async (id: number, request: APIRequestContext): Promise<void> => {
-    await request.delete(`http://localhost:3001/users/${id}`)
+    await request.delete(`http://localhost:3000/users/${id}`)
   }
 
   test('TL-14-1 create user test', async ({ request }) => {
-    const creationResponse = await request.post('http://localhost:3001/users')
+    const creationResponse = await request.post('http://localhost:3000/users')
     expect(creationResponse.status()).toBe(StatusCodes.CREATED)
 
     const createdUser = UserDTO.serializeResponse(await creationResponse.json())
@@ -19,9 +19,9 @@ test.describe('User managment app tests', async () => {
   })
 
   test('TL-14-2 find user test', async ({ request }) => {
-    const creationResponse = await request.post('http://localhost:3001/users')
+    const creationResponse = await request.post('http://localhost:3000/users')
     const createdUser = UserDTO.serializeResponse(await creationResponse.json())
-    const searchResponse = await request.get(`http://localhost:3001/users/${createdUser.id}`)
+    const searchResponse = await request.get(`http://localhost:3000/users/${createdUser.id}`)
     const foundUser = UserDTO.serializeResponse(await searchResponse.json())
 
     expect(foundUser).toStrictEqual(createdUser)
@@ -30,19 +30,19 @@ test.describe('User managment app tests', async () => {
   })
 
   test('TL-14-3 delete user test', async ({ request }) => {
-    const creationResponse = await request.post('http://localhost:3001/users')
+    const creationResponse = await request.post('http://localhost:3000/users')
     const createdUser = UserDTO.serializeResponse(await creationResponse.json())
-    const deleteResponse = await request.delete(`http://localhost:3001/users/${createdUser.id}`)
+    const deleteResponse = await request.delete(`http://localhost:3000/users/${createdUser.id}`)
     const deletedUser = UserDTO.serializeResponse((await deleteResponse.json())[0])
 
     expect(deletedUser).toStrictEqual(createdUser)
   })
 
   test('TL-14-4 get all users test', async ({ request }) => {
-    await request.post('http://localhost:3001/users')
-    await request.post('http://localhost:3001/users')
-    await request.post('http://localhost:3001/users')
-    const allUsersResponse = await request.get('http://localhost:3001/users')
+    await request.post('http://localhost:3000/users')
+    await request.post('http://localhost:3000/users')
+    await request.post('http://localhost:3000/users')
+    const allUsersResponse = await request.get('http://localhost:3000/users')
     const json: UserDTO[] = await allUsersResponse.json()
 
     expect(json.length).toBe(3)
