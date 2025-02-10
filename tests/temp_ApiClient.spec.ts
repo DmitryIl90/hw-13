b30cee363a2717e9939cc0a384d9cd0d3683f7b1
AndrewKhv <a.d.khramchenkov@gmail.com>
2025-02-10 20:21:01 +0100

    feat: TL-14 added local api servise

--- tests/temp_ApiClient.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test'
-import { Temp_ApiClient } from '../api/temp_ApiClient'
-
-test('login and create order with api client', async ({ request }) => {
-  const apiClient = await Temp_ApiClient.getInstance(request)
-  const orderId = await apiClient.createOrderAndReturnOrderId()
-  console.log('orderId:', orderId)
-})
