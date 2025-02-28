7ef6487fbc5fcdcc2f0d6d3b719c4b2ee2e33e26
Dmitry Ilves <ilvesdmitry@gmail.com>
2025-02-28 21:03:17 +0200

    changed yml file and created GetAllUsers.spec.ts

---
+++ tests/GetAllUsers.spec.ts
@@ -0,0 +1,12 @@
+import { UserDTO } from './DTO/UserDTO'
+import { expect, test } from '@playwright/test'
+
+test('TL-14-4 get all users test', async ({ request }) => {
+  await request.post('http://localhost:3000/users')
+  await request.post('http://localhost:3000/users')
+  await request.post('http://localhost:3000/users')
+  const allUsersResponse = await request.get('http://localhost:3000/users')
+  const json: UserDTO[] = await allUsersResponse.json()
+
+  expect(json.length).toBe(3)
+})
